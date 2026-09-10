165bc5b2e8d08c21decc11e3f3c2e8d1df252475
Sandeep <sandeepmoji1@gmail.com>
2026-09-10 07:15:00 +0000

    Name of the Project Updated

--- backend/NammaMitra/asgi.py
+++ backend/NammaMitra/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for NammaHakku project.
+ASGI config for NammaMitra project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'NammaHakku.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'NammaMitra.settings')
 
 application = get_asgi_application()
